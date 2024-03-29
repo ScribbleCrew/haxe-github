159652c8b55da969c5f958a55f7590305fb484e2
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-03-28 22:08:06 -0300

    More Functions and Fix the Repository Util

--- haxegithub/utils/Repository.hx
+++ haxegithub/utils/Repository.hx
@@ -24,6 +24,19 @@ class Repository {
 	 */
 	public static function getContributors(user:String, repo:String):Dynamic {
 		var api = new GithubAPI();
+		api.request('repos/$user/$repo/contributors');
+		return api.json;
+	}
+
+	/**
+	 * get Collaborators from a Repository, Requires a Token!
+	 * @param token
+	 * @param user 
+	 * @param repo 
+	 * @return api.json
+	 */
+	public static function getCollaborators(token:String, user:String, repo:String):Dynamic {
+		var api = new GithubAPI(token);
 		api.request('repos/$user/$repo/collaborators');
 		return api.json;
 	}
@@ -33,7 +46,7 @@ class Repository {
 	 * @param user 
 	 * @param repo 
 	 */
-	 public static function getReleases(user:String, repo:String):Dynamic {
+	public static function getReleases(user:String, repo:String):Dynamic {
 		var api = new GithubAPI();
 		api.request('repos/$user/$repo/releases');
 		return api.json;
