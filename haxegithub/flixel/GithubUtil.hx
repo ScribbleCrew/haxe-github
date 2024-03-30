44e0284501682e859d1f807ad6a2b073a998264b
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-03-30 11:17:18 -0300

    Flixel

---
+++ haxegithub/flixel/GithubUtil.hx
@@ -0,0 +1,15 @@
+package haxegithub.flixel;
+
+#if flixel
+import flixel.FlxG;
+import openfl.display.BitmapData;
+
+class GithubUtil {
+	public static function image(imageUrl:String, ?unique:Bool = false, ?key:String = '') {
+		var api = new Http(imageUrl); // Custom Method
+		var responseBytes = new haxe.io.BytesOutput();
+		api.request();
+		return FlxG.bitmap.add(BitmapData.fromBytes(responseBytes.getBytes()), unique, key);
+	}
+}
+#end
