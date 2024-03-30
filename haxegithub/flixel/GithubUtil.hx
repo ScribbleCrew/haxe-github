940ad2da2121864ce80314fb6a3d4b5c560f70d1
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-03-30 11:25:59 -0300

    aaaaaaa More Fixes

--- haxegithub/flixel/GithubUtil.hx
+++ haxegithub/flixel/GithubUtil.hx
@@ -8,9 +8,8 @@ import haxe.Http;
 class GithubUtil {
 	public static function image(imageUrl:String, ?unique:Bool = false, ?key:String = '') {
 		var api = new Http(imageUrl); // Custom Method
-		var responseBytes = new haxe.io.BytesOutput();
 		api.request();
-		return FlxG.bitmap.add(BitmapData.fromBytes(responseBytes.getBytes()), unique, key);
+		return FlxG.bitmap.add(BitmapData.fromBytes(api.responseBytes), unique, key);
 	}
 }
 #end
